f0392b58a82644a6da917e7d5b8b358fcb0a8e15
Guilherme Taschetto <gtaschetto@gmail.com>
2014-03-20 20:00:29 -0300

    Add contractProgramming exercises

---
+++ contractProgramming/src/ex01/IFifo.java
@@ -0,0 +1,32 @@
+package ex01;
+
+public interface IFifo {
+	
+	//@ requires size() < capacity();
+	//@ requires size() == 0 || (tail() + n == 0) || (tail() >= 0 && n >= 0) || (tail() < 0 && n < 0);
+	//@ ensures size() == \old(size()) + 1;
+	//@ ensures tail() == n;
+	void enqueue(int n);
+	
+	//@ requires size() > 0;
+	//@ ensures size() == \old(size()) - 1;
+	//@ ensures \result == \old(head());
+	int dequeue();
+	
+	//@ requires size() > 0;
+	//@ pure
+	int head();
+	
+	//@ requires size() > 0;
+	//@ pure
+	int tail();	
+	
+	//@ pure
+	boolean isEmpty();
+	
+	//@ pure
+	int size();
+	
+	//@ pure
+	int capacity();
+}
